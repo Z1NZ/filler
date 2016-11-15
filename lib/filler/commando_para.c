451eb1e7e49200fff547f879e27617d5322e6043
z1nz <srabah@student.42.fr>
2016-11-15 03:15:15 +0100

    fixup! fixup! fixup! fixup! check ok

--- lib/filler/commando_para.c
+++ lib/filler/commando_para.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   commando_para.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: srabah <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: srabah <srabah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/26 07:00:44 by srabah            #+#    #+#             */
-/*   Updated: 2016/10/26 07:00:45 by srabah           ###   ########.fr       */
+/*   Updated: 2016/11/11 14:02:29 by srabah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-int		claymore(t_data *data, int pos_x, int pos_y, char min, char maj)
+int		claymore(t_data *data, int pos_x, int pos_y, char min, char maj, int save_x, int save_y)
 {
 	char	**tmp;
 	char	**tab;
@@ -24,14 +24,16 @@ int		claymore(t_data *data, int pos_x, int pos_y, char min, char maj)
 	y = 0;
 	tmp = data->piece.piece;
 	tab = data->map;
-	while (tmp[y] && tab[pos_y + y])
+	while ((y + save_y) < data->piece.y && pos_y + y < data->y)
 	{
 		x = 0;
-		while (y < data->piece.y && tmp[y][x] && tab[pos_y + y][pos_x + x])
+		while ((x + save_x) < data->piece.x && (pos_x + x) < data->x)
 		{
-			if (tmp[y][x] == tab[pos_y + y][pos_x + x] || ((tmp[y][x] == '*') && tab[pos_y + y][pos_x + x] == '.'))
+			if (tmp[y + save_y][x + save_x] == tab[pos_y + y][pos_x + x] 
+				|| ((tmp[y + save_y][x + save_x] == '*') && tab[pos_y + y][pos_x + x] == '.'))
 				++x;
-			else if ((tab[pos_y + y][pos_x + x] == maj || tab[pos_y + y][pos_x + x] == min) && (tmp[y][x] == '*'))
+			else if ((tab[pos_y + y][pos_x + x] == maj || tab[pos_y + y][pos_x + x] == min) &&
+				(tmp[y + save_y][x + save_x] == '*'))
 			{
 				touch++;
 				++x;
@@ -43,10 +45,12 @@ int		claymore(t_data *data, int pos_x, int pos_y, char min, char maj)
 	}
 	if (touch == 1)
 		return (1);
+	// if (pos_y == 0)
+	// fprintf(stderr, "pos_y=[%d]	pos_x=[%d]	save_x=[%d]	save_y=[%d]	touch=[%d]\n", pos_y, pos_x, save_x, save_y, touch);
 	return (0);
 }
 
-void	supervision(t_data *data, char maj, char min)
+void	supervision(t_data *data, char maj, char min, int save_x, int save_y)
 {
 	char	**tmp;
 	int		x;
@@ -57,105 +61,56 @@ void	supervision(t_data *data, char maj, char min)
 	while (tmp[y])
 	{
 		x = 0;
-		while (tmp[y][x])
+		while (tmp[y][x] && x < (data->x - 1))
 		{
-			if (claymore(data, x, y, min, maj) == 1)
-				add_pos(data, x, y);
+			if (claymore(data, x, y, min, maj, save_x, save_y) == 1)
+				add_pos(data, x - save_x, y - save_y);
 			++x;
 		}
 		++y;
 	}
 }
 
-void	ft_finisher(t_data *data, int s_y, int s_x, int start)
-{
-	char	**tab;
-	int		x;
-	int		y;
-	int		super;
-
-	x = 0;
-	y = 0;
-	tab = ft_memalloc(sizeof(char *) * s_y);
-	super = 0;
-	while (y < data->piece.y && data->piece.piece[y])
-	{
-		x = 0;
-		while (data->piece.piece[y][x] != '\0' && data->piece.piece[y][x] == '.')
-			x++;
-		if (data->piece.piece[y][x] != '\0')
-		{
-			tab[super] = ft_strdup(ft_strsub(data->piece.piece[y], start, s_x));
-			++super;
-		}
-		y++;
-	}
-	data->piece.y = s_y;
-	data->piece.x = s_x;
-	ft_memdel((void**)&data->piece.piece);
-	data->piece.piece = tab;
-}
 
 void	ft_super_wod(t_data *data)
 {
 	int y;
+	int save_y;
 	int x;
-	int size_y;
-	int size_x;
-	int start;
+	int save_x;
 
-	y = 0;
-	size_y = data->piece.y;
-	size_x = 1;
-	start = 0;
-	while (y < data->piece.y)
+	save_x = INT_MAX;
+	y = -1;
+	while(ft_strstr(data->piece.piece[++y],"*") == NULL)
+		;
+	save_y = y;
+	--y;
+	while(data->piece.piece[++y])
 	{
-		x = 0;
-		while (data->piece.piece[y][x] && data->piece.piece[y][x] == '.')
-			x++;
-		if (data->piece.piece[y][x] == '\0')
-			--size_y;
-		if (start == 0)
+		x = -1;
+		while(data->piece.piece[y][++x])
 		{
-			data->piece.org_y = data->piece.y - size_y;
-			++start;
+			if (data->piece.piece[y][x] == '*' && x < save_x)
+				save_x = x;
 		}
-		y++;
 	}
-	y = 0;
-	start = INT_MAX;
-	while (y < data->piece.y)
-	{
-		x = 0;
-		while (data->piece.piece[y][x])
-		{
-			if (data->piece.piece[y][x] == '*' && x < start)
-				start = x;
-			if (data->piece.piece[y][x] == '*' && x > size_x)
-				size_x = x;
-			x++;
-		}
-		y++;
-	}
-	data->piece.org_x = start;
-	ft_finisher(data, size_y, size_x, start);
+	if (CHECK_BIT(data->status, OPT_PLAYER1))
+		supervision(data, 'O', 'o', save_x, save_y);
+	else
+		supervision(data, 'X', 'x', save_x, save_y);
 }
 
 void	commando_para(t_data *data)
 {
 	ft_super_wod(data);
-	if (CHECK_BIT(data->status, OPT_PLAYER1))
-		supervision(data, 'O', 'o');
-	else
-		supervision(data, 'X', 'x');
-	fprintf(stderr, "------------[Y + %d]-----------------\n", data->piece.org_y);
-	fprintf(stderr, "------------[X - %d]-----------------\n", data->piece.org_x);
 	if (data->pos)
 	{
-		ft_putnbr(data->pos->y + data->piece.org_y);
+		ft_putnbr(data->pos->y);
 		write(1, " ", 1);
-		ft_putnbr(data->pos->x - data->piece.org_x);
+		ft_putnbr(data->pos->x);
 		write(1, "\n", 1);
 	}
+	else
+		write(1, "0 0\n", 4);
 	ft_free(data);
 }
