a030d9ae22fb101dbd9cc5151a16a65103c07c3c
z1nz <sofiane.rabah@gmail.com>
2017-04-09 14:40:59 +0200

    p1 a optimiser

--- lib/filler/algo.c
+++ lib/filler/algo.c
@@ -1,5 +1,92 @@
 #include "filler.h"
 
+
+
+
+
+int size_x_real_piece(t_data *data)
+{
+	int x;
+	int y;
+	int size_x;
+	char **tab;
+
+	tab = data->piece.piece;
+	y = 0;
+	size_x = 0;
+	while(tab[y])
+	{
+		x = 0;
+		while(tab[y][x])
+		{
+			if (tab[y][x] ==  '*' &&  x > size_x)
+				size_x = x;
+			x++;
+		}
+		y++;
+	}
+	return(size_x);
+}
+
+
+int size_y_real_piece(t_data *data)
+{
+	char **tab;
+	int y;
+	int size_y;
+
+	tab = data->piece.piece;
+	y = 0;
+	size_y = 0;
+	while(tab[y])
+	{
+		if (ft_strstr(tab[y], "*"))
+			size_y++;
+		y++;
+	}
+	return(size_y);
+}
+
+
+t_pos		*close_position(t_data *data)
+{
+	t_pos	*tmp;
+	int		y;
+	int		x;
+
+	y = 0;
+	while(data->map.map[y])
+	{
+		x = 0;
+		while(data->map.map[x])
+		{
+			if (data->map.map[y][x] == 'x')
+				break;
+			x++;
+		}
+		y++;
+	}
+	tmp = data->pos;
+	int old_x = 0;
+	int old_y = 0;
+	t_pos *ret;
+	int len_x_piece = size_x_real_piece(data);
+	int len_y_piece = size_y_real_piece(data);
+
+	ret = NULL;
+	while(tmp)
+	{
+		if ((ABS(tmp->x + len_x_piece ) - x < old_x ) && ABS((tmp->y + len_y_piece ) - y ) < old_y)
+			ret = tmp;
+		tmp = tmp->next;
+	}
+	return(ret);
+}
+
+
+
+
+
 int min_pos_x(t_data *data)
 {
 	int y;
@@ -13,7 +100,7 @@ int min_pos_x(t_data *data)
 		x = 0;
 		while(data->map.map[x])
 		{
-			if (data->map.map[y][x] == 'X' && x < ret_x)
+			if (data->map.map[y][x] == 'O' && x < ret_x)
 				ret_x = x;
 			x++;
 		}
@@ -122,11 +209,17 @@ t_pos	*algo_first(t_data *data)
 	max_y = max_pos_y(data);
 
 	if (max_x == data->map.x || max_y == data->map.y)
+	{
 		return(top_list(data, LEFT));
+	}
 	else if (max_x < data->map.x)
+	{
 		return(bot_list(data, RIGHT));
+	}
 	else
+	{
 		return(bot_list(data, LEFT));
+	}
 }
 
 t_pos		*algo(t_data *data)
